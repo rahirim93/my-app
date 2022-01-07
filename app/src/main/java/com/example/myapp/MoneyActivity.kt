dab087145d355dec0c3f5067f7baa18cc8107433
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2022-01-07 10:53:14 +0300

    Добавлена функция подсчета количество денег которые можно потратить чтобы уложится в выбранную сумму в месяц

--- app/src/main/java/com/example/myapp/MoneyActivity.kt
+++ app/src/main/java/com/example/myapp/MoneyActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.widget.*
 import java.util.*
 
+
+
 class MoneyActivity : AppCompatActivity() {
     private lateinit var editTextMoney: EditText
     private lateinit var checkBox: CheckBox
@@ -18,6 +20,8 @@ class MoneyActivity : AppCompatActivity() {
     private lateinit var textViewSeekBarProgress: TextView
     private lateinit var textViewPref: TextView
     private lateinit var textViewSeekBar2: TextView
+    private lateinit var editTextMoneyWished: EditText
+    private lateinit var textViewMoneyAvailable : TextView
 
     private var pref: SharedPreferences? = null
 
@@ -32,6 +36,7 @@ class MoneyActivity : AppCompatActivity() {
         //Подгрузка сохраненного состояния
         textViewPref.text = pref?.getInt("money", 0).toString()
         editTextMoney.setText(pref?.getInt("money", 0).toString())
+        editTextMoneyWished.setText(pref?.getInt("moneyWished", 0).toString())
 
         //Подгрузка сохраненного состояния checkBox при запуске
         var stateOfCheckBox = pref?.getBoolean("checkBox", false)
@@ -49,6 +54,9 @@ class MoneyActivity : AppCompatActivity() {
         textViewSeekBarProgress = findViewById(R.id.textViewSeekBarProgress)
         textViewPref = findViewById(R.id.textViewPref)
         textViewSeekBar2 = findViewById(R.id.textViewSeekBar2)
+        editTextMoneyWished = findViewById(R.id.editTextMoneyWished)
+        textViewMoneyAvailable = findViewById(R.id.textViewMoneyAvailable)
+
 
         pref = getSharedPreferences("Name", Context.MODE_PRIVATE)
     }
@@ -90,6 +98,8 @@ class MoneyActivity : AppCompatActivity() {
     }
 
     private fun countMoney(todayDay: Int) {
+        val rentPay = 21000 // Аренда квартиры
+
         //Получение и приведение значения в поле к типу Int
         val money = (editTextMoney.text).toString().toInt()
 
@@ -105,24 +115,36 @@ class MoneyActivity : AppCompatActivity() {
         textViewMoneyADay.text = "Денег в день: $moneyOfDay"    // Выводим в textView
 
         // Количество денег в месяц с учетом квартплаты
-        val moneyMonth = (moneyOfDay * countDayMonth) + 21000   // Количество денег в месяц вместе с квартплатой
+        val moneyMonth = (moneyOfDay * countDayMonth) + rentPay   // Количество денег в месяц вместе с квартплатой
         textViewMoneyAMonth.text = "Денег в месяц: $moneyMonth"
+
+        // Подсчет и вывод количества денег, которые можно потратить сегодня
+        //Получение и приведение значения в поле к типу Int
+        val moneyWished = (editTextMoneyWished.text).toString().toInt() // Желаемая сумма в месяц
+
+        if(moneyWished > 0) {
+            val moneyAvailable = (moneyWished - rentPay) / 31 * todayDay - money //Сколько можно потратить сегодня
+            textViewMoneyAvailable.text = "Можно потратить: $moneyAvailable"
+        }
+
+
     }
 
-    private fun saveMoneyPref(money: Int, checkBox: Boolean) {
+    private fun saveMoneyPref(money: Int, checkBox: Boolean, moneyWished: Int) {
         val editor = pref?.edit()
         editor?.putInt("money", money)
         editor?.putBoolean("checkBox", checkBox)
+        editor?.putInt("moneyWished", moneyWished)
         editor?.apply()
     }
 
     override fun onStop() {
         super.onStop()
-        saveMoneyPref(editTextMoney.text.toString().toInt(), checkBox.isChecked)
+        saveMoneyPref(editTextMoney.text.toString().toInt(), checkBox.isChecked, editTextMoneyWished.text.toString().toInt())
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        saveMoneyPref(editTextMoney.text.toString().toInt(), checkBox.isChecked)
+        saveMoneyPref(editTextMoney.text.toString().toInt(), checkBox.isChecked, editTextMoneyWished.text.toString().toInt())
     }
 }
