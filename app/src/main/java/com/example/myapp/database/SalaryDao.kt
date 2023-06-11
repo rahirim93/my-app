b8e0518e706881f77b5df2664b837249227a9556
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2023-06-11 13:20:57 +0300

    #41. База данных зарплаты.
    Изменена сортировка в списке на по убыванию по времени.

--- app/src/main/java/com/example/myapp/database/SalaryDao.kt
+++ app/src/main/java/com/example/myapp/database/SalaryDao.kt
@@ -6,7 +6,7 @@ import androidx.room.*
 @Dao
 interface SalaryDao {
 
-    @Query("SELECT * FROM SalaryEntity ORDER BY date ASC")
+    @Query("SELECT * FROM SalaryEntity ORDER BY date DESC")
     fun getSalaries(): LiveData<List<SalaryEntity>>
 
     @Query("SELECT * FROM SalaryEntity WHERE id = :id")
